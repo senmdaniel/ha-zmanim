80149ae0f7312bd9e43a9c59cd290b9dd5ec5c58
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 00:18:37 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,6 +1,21 @@
+from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
 DOMAIN = "zmanim"
 
+PLATFORMS = ["sensor"]
+
+
 async def async_setup(hass: HomeAssistant, config: dict):
+    return True
+
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+):
+    await hass.config_entries.async_forward_entry_setups(
+        entry,
+        PLATFORMS,
+    )
     return True
