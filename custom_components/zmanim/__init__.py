c0302a4a6146e64d2aeaf28eb9c4fc2540f408d5
senmdaniel <senmdaniel@hotmail.com>
2026-05-31 20:16:19 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,1 +1,6 @@
-"""HA Zmanim Pro"""
+from datetime import date
+from .app.core.zmanim import calculate_zmanim
+
+async def async_setup(hass, config):
+    hass.data["zmanim"] = calculate_zmanim({}, date.today())
+    return True
