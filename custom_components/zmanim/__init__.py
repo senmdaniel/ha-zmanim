27555b5265a68801570e32f4d3feae3bdbb9df1e
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 00:12:34 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,9 +1,6 @@
 from homeassistant.core import HomeAssistant
-from .const import DOMAIN
 
 DOMAIN = "zmanim"
 
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN]["loaded"] = True
+async def async_setup(hass: HomeAssistant, config: dict):
     return True
