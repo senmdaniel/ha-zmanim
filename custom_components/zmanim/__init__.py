5ba8582f1d0eb69bf37564bd6d1fee4d96754033
senmdaniel <senmdaniel@hotmail.com>
2026-05-31 21:48:01 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,12 +1,1 @@
-import logging
-from datetime import date
-from .app.core.zmanim import calculate_zmanim
-
-_LOGGER = logging.getLogger(__name__)
-
-async def async_setup(hass, config):
-    _LOGGER.warning("ZMANIM INIT LOADED")
-
-    hass.data["zmanim"] = calculate_zmanim({}, date.today())
-
-    return True
+config/custom_components/test_sensor/__init__.py
