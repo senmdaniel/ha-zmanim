938d48dcf0a0f62d7a1e3a744c5823879dd68750
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 00:15:44 +0200

    Create config_flow.py

---
+++ custom_components/zmanim/config_flow.py
@@ -0,0 +1,20 @@
+from homeassistant import config_entries
+from .const import DOMAIN
+
+
+class ZmanimConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Zmanim."""
+
+    VERSION = 1
+
+    async def async_step_user(self, user_input=None):
+        if user_input is not None:
+            return self.async_create_entry(
+                title="Zmanim",
+                data=user_input,
+            )
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=None,
+        )
