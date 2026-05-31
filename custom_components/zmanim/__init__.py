68355b2d1675df5ff25c9958016d5bcd2ee1ffae
senmdaniel <senmdaniel@hotmail.com>
2026-05-31 21:48:26 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,1 +1,5 @@
-config/custom_components/test_sensor/__init__.py
+from homeassistant.core import HomeAssistant
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    hass.states.async_set("test_sensor.hello", "working")
+    return True
