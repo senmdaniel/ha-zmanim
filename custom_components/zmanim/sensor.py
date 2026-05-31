9df7ebdc87a320dbbade0640715a8fd8f9c6103d
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 00:03:14 +0200

    Update sensor.py

--- custom_components/zmanim/sensor.py
+++ custom_components/zmanim/sensor.py
@@ -1,29 +1,18 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .zmanim import get_zman_times
-
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
+async def async_setup_platform(
+    hass,
+    config,
+    async_add_entities,
+    discovery_info=None,
 ):
     async_add_entities([ZmanimSensor()])
 
-class ZmanimSensor(SensorEntity):
 
+class ZmanimSensor(SensorEntity):
     _attr_name = "Zmanim Test"
     _attr_unique_id = "zmanim_test"
 
-    def __init__(self):
-        self._state = None
-
     @property
     def native_value(self):
-        return self._state
-
-    async def async_update(self):
-        data = get_zman_times("Amsterdam")
-        self._state = data["sunrise"]
+        return "TEST-06:00"
