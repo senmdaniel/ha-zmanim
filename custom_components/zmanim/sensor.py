3b55169beb58cc9000f5afa0b68c9dcc4288da28
senmdaniel <senmdaniel@hotmail.com>
2026-05-31 23:51:14 +0200

    Update sensor.py

--- custom_components/zmanim/sensor.py
+++ custom_components/zmanim/sensor.py
@@ -1,24 +1,27 @@
-from homeassistant.helpers.entity import Entity
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
 from .zmanim import get_zman_times
 
-async def async_setup_platform(hass, config, add_entities, discovery_info=None):
-    add_entities([ZmanimSensor()])
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    async_add_entities([ZmanimSensor()])
 
-class ZmanimSensor(Entity):
+class ZmanimSensor(SensorEntity):
+
+    _attr_name = "Zmanim Test"
+    _attr_unique_id = "zmanim_test"
 
     def __init__(self):
         self._state = None
 
     @property
-    def name(self):
-        return "Zmanim Test"
-
-    @property
-    def unique_id(self):
-        return "zmanim_test"
-
-    @property
-    def state(self):
+    def native_value(self):
         return self._state
 
     async def async_update(self):
