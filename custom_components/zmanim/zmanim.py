106c0039719a0f8c3a53bb1cdc9e02502c732bd7
senmdaniel <senmdaniel@hotmail.com>
2026-05-31 23:38:31 +0200

    Update zmanim.py

--- custom_components/zmanim/zmanim.py
+++ custom_components/zmanim/zmanim.py
@@ -1,19 +1,6 @@
-from datetime import datetime
-
 def get_zman_times(location="Amsterdam"):
-    """
-    Simpele demo Zmanim functie.
-    Later kun je hier echte berekeningen toevoegen.
-    """
-
-    now = datetime.now()
-
-    # PLACEHOLDER times (later vervangen door echte berekening)
     return {
-        "location": location,
-        "date": now.strftime("%Y-%m-%d"),
-        "sunrise": "06:42",
-        "sunset": "20:15",
-        "shabbat_start": "18:45",
-        "shabbat_end": "21:30"
+        "sunrise": "TEST-06:00",
+        "sunset": "TEST-20:00",
+        "location": location
     }
