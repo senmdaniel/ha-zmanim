186cafdfac623f803b86ec9363348c6654afdf69
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 11:00:48 +0200

    Update sensor.py

--- custom_components/zmanim/sensor.py
+++ custom_components/zmanim/sensor.py
@@ -1,47 +1,43 @@
-from datetime import date
-from homeassistant.components.sensor import SensorEntity
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.entity import Entity
+from homeassistant.helpers.update_coordinator import CoordinatorEntity
+from .const import DOMAIN
 
-from .core.zmanim_engine import calculate_zmanim
+ZMANIM_KEYS = [
+    "alot_hashachar",
+    "netz",
+    "chatzot",
+    "shkia",
+    "tzeit",
+]
 
-DOMAIN = "zmanim"
+async def async_setup_entry(hass, entry, async_add_entities):
+    coordinator = hass.data[DOMAIN][entry.entry_id]
+
+    entities = [
+        ZmanimSensor(coordinator, key)
+        for key in ZMANIM_KEYS
+    ]
+
+    async_add_entities(entities)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-    async_add_entities: AddEntitiesCallback,
-):
-    async_add_entities([ZmanimSensor(entry.entry_id)])
-
-
-class ZmanimSensor(SensorEntity):
-    def __init__(self, entry_id: str):
-        self._entry_id = entry_id
-        self._attr_name = "Zmanim Test"
-        self._attr_unique_id = f"zmanim_{entry_id}"
-
-    @property
-    def native_value(self):
-        config = {
-            "city": "Antwerp",
-            "timezone": "Europe/Brussels",
-            "latitude": 51.2194,
-            "longitude": 4.4025,
-        }
-
-        data = calculate_zmanim(config, date.today())
-        return data["zmanim"]["shkia"]["time"]
+class ZmanimSensor(CoordinatorEntity, Entity):
+    def __init__(self, coordinator, key):
+        super().__init__(coordinator)
+        self._key = key
 
     @property
-    def extra_state_attributes(self):
-        config = {
-            "city": "Antwerp",
-            "timezone": "Europe/Brussels",
-            "latitude": 51.2194,
-            "longitude": 4.4025,
-        }
+    def name(self):
+        return f"Zmanim {self._key}"
 
-        return calculate_zmanim(config, date.today())
+    @property
+    def unique_id(self):
+        return f"zmanim_{self._key}"
+
+    @property
+    def state(self):
+        return self.coordinator.data.get(self._key)
+
+    @property
+    def device_class(self):
+        return "timestamp"
