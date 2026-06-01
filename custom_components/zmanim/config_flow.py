ffcfca35ad1e584e2d8301d2ba081366eb86b9a4
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 11:01:17 +0200

    Update config_flow.py

--- custom_components/zmanim/config_flow.py
+++ custom_components/zmanim/config_flow.py
@@ -1,24 +1,20 @@
 import voluptuous as vol
 from homeassistant import config_entries
-
-from .const import DOMAIN
-
+from .const import DOMAIN, DEFAULT_METHOD
 
 class ZmanimConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Handle a config flow for Zmanim."""
-
-    VERSION = 1
 
     async def async_step_user(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(
                 title="Zmanim",
-                data=user_input or {},
+                data=user_input,
             )
 
-        schema = vol.Schema({})
+        schema = vol.Schema({
+            vol.Required("latitude"): float,
+            vol.Required("longitude"): float,
+            vol.Optional("method", default=DEFAULT_METHOD): str,
+        })
 
-        return self.async_show_form(
-            step_id="user",
-            data_schema=schema,
-        )
+        return self.async_show_form(step_id="user", data_schema=schema)
