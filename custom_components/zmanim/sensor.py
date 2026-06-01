e79357e74ed635e3bb1316b5fac3c59e5da68f86
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 09:24:29 +0200

    Update sensor.py

--- custom_components/zmanim/sensor.py
+++ custom_components/zmanim/sensor.py
@@ -1,17 +1,21 @@
+from datetime import datetime
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-DOMAIN = "zmanim"
+from .core.hebrew_calendar import get_hebrew_date
 
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([ZmanimSensor()])
+    async_add_entities([HebrewDateSensor()])
 
 
-class ZmanimSensor(SensorEntity):
-    _attr_name = "Zmanim Test"
-    _attr_unique_id = "zmanim_test"
+class HebrewDateSensor(SensorEntity):
+    _attr_name = "Hebrew Date"
+    _attr_unique_id = "hebrew_date"
 
     @property
     def native_value(self):
-        return "OK"
+        now = datetime.now()
+
+        result = get_hebrew_date(now)
+
+        return f"{result['hebrew_day']}/{result['hebrew_month']}/{result['hebrew_year']}"
