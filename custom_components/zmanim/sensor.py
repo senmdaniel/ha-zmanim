5dfe1469476ca22978f5e6ff0d434470234311cf
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 09:44:21 +0200

    Update sensor.py

--- custom_components/zmanim/sensor.py
+++ custom_components/zmanim/sensor.py
@@ -1,32 +1,47 @@
 from datetime import date
 from homeassistant.components.sensor import SensorEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .core.zmanim_engine import calculate_zmanim
 
-
-DEFAULT_CONFIG = {
-    "city": "Antwerp",
-    "timezone": "Europe/Brussels",
-    "latitude": 51.2194,
-    "longitude": 4.4025,
-}
+DOMAIN = "zmanim"
 
 
-async def async_setup_entry(hass, entry, async_add_entities):
-    async_add_entities([ZmanimSensor()])
+async def async_setup_entry(
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+):
+    async_add_entities([ZmanimSensor(entry.entry_id)])
 
 
 class ZmanimSensor(SensorEntity):
-    _attr_name = "Zmanim"
-    _attr_unique_id = "zmanim_engine"
+    def __init__(self, entry_id: str):
+        self._entry_id = entry_id
+        self._attr_name = "Zmanim Test"
+        self._attr_unique_id = f"zmanim_{entry_id}"
 
     @property
     def native_value(self):
-        data = calculate_zmanim(DEFAULT_CONFIG, date.today())
+        config = {
+            "city": "Antwerp",
+            "timezone": "Europe/Brussels",
+            "latitude": 51.2194,
+            "longitude": 4.4025,
+        }
 
-        # simpele “main value”
+        data = calculate_zmanim(config, date.today())
         return data["zmanim"]["shkia"]["time"]
 
     @property
     def extra_state_attributes(self):
-        return calculate_zmanim(DEFAULT_CONFIG, date.today())
+        config = {
+            "city": "Antwerp",
+            "timezone": "Europe/Brussels",
+            "latitude": 51.2194,
+            "longitude": 4.4025,
+        }
+
+        return calculate_zmanim(config, date.today())
