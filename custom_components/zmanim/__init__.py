cb303bc366a06ca62ed07cfd2881994313b42abd
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 11:06:52 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,6 +1,5 @@
 from homeassistant.core import HomeAssistant
 from .const import DOMAIN
-from .coordinator import ZmanimCoordinator
 
 async def async_setup_entry(hass: HomeAssistant, entry):
     hass.data.setdefault(DOMAIN, {})
@@ -9,10 +8,13 @@ async def async_setup_entry(hass: HomeAssistant, entry):
     lon = entry.data["longitude"]
     method = entry.data.get("method", "gra")
 
+    from .coordinator import ZmanimCoordinator
+
     coordinator = ZmanimCoordinator(hass, lat, lon, method)
     await coordinator.async_config_entry_first_refresh()
 
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+
     return True
