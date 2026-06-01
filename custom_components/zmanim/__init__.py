12880813c3d797afcc60b65d0a39658df95ab096
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 09:25:56 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,32 +1,5 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-
 PLATFORMS = ["sensor"]
 
-
-async def async_setup(hass: HomeAssistant, config: dict) -> bool:
-    """Set up the Zmanim integration."""
+async def async_setup_entry(hass, entry):
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
-
-
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Set up Zmanim from a config entry."""
-    await hass.config_entries.async_forward_entry_setups(
-        entry,
-        PLATFORMS,
-    )
-    return True
-
-
-async def async_unload_entry(
-    hass: HomeAssistant,
-    entry: ConfigEntry,
-) -> bool:
-    """Unload a config entry."""
-    return await hass.config_entries.async_unload_platforms(
-        entry,
-        PLATFORMS,
-    )
