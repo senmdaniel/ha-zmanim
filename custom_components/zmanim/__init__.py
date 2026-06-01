6e9dcbb699642eff04aa058133756d69736ddeaf
senmdaniel <senmdaniel@hotmail.com>
2026-06-01 13:36:05 +0200

    Update __init__.py

--- custom_components/zmanim/__init__.py
+++ custom_components/zmanim/__init__.py
@@ -1,14 +1,9 @@
 import logging
 from .const import DOMAIN
-
 from .api import ZmanimView
 
-async def async_setup_entry(hass, entry):
-    ...
-    hass.http.register_view(ZmanimView)
-    return True
-    
 _LOGGER = logging.getLogger(__name__)
+
 
 async def async_setup_entry(hass, entry):
 
@@ -23,6 +18,9 @@ async def async_setup_entry(hass, entry):
         await coordinator.async_config_entry_first_refresh()
 
         hass.data[DOMAIN][entry.entry_id] = coordinator
+
+        # 🔥 REGISTER API VIEW (BELANGRIJK)
+        hass.http.register_view(ZmanimView)
 
         await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
 
